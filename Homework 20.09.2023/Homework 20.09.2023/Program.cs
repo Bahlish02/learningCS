b387ebf1ffc3a5f960834cdfc4627c52dc68999d
tagir_student_itis@mail.ru <tagir_student_itis@mail.ru>
2023-09-18 21:52:42 +0300

    Homework

---
+++ Homework 20.09.2023/Homework 20.09.2023/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int Num = int.Parse(Console.ReadLine());
+        int SecNum = Num;
+        if (Num % 3 == 0)
+        {
+            int CounterOfDegree = 0;
+            while ((Num % 3 == 0) && (Num != 1))
+            {
+                Num = Num / 3;
+                //Console.WriteLine(Num);
+                CounterOfDegree += 1;
+            }
+            if (Num % 3 == 1 && Math.Pow(3, CounterOfDegree) == SecNum)
+            {
+                Console.WriteLine("Degree of 3");
+            }
+            else
+            {
+                Console.WriteLine("Not a degree of 3");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not degree of 3");
+        }
+    }
+}
